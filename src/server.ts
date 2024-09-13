b90974bda8dafa864c83edcaec404e71aa12a0dd
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-09-13 11:51:28 -0300

    feat: Add transactions table

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const tables = await knex('sqlite_schema').select('*')
 
-  return tables
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Test transaction',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
